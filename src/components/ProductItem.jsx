5afbbab6ec998c37c527b26a12367772c733ab17
Mangesh2042 <mangesh2042@gmail.com>
2022-06-20 21:11:42 +0530

    add remove completed

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,60 @@
 import React from "react";
 
-const ProductItem = () => {
-  return <div>{/* CODE HERE */}</div>;
+const ProductItem = ({e}) => 
+{
+  // id: 9, name: 'Product 9', description: 'Product 9 Description'
+
+  const addHanddle=()=>
+  {
+    const data=e
+
+    fetch('http://localhost:8080/cartItems', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data); 
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+
+  const removeHanddle=()=>
+  {
+    const data=e
+    fetch(`http://localhost:8080/cartItems/${data.id}`, {
+      method: 'DELETE', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data); 
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+  
+  return <div>{
+    <>
+      <div key={e.id}>
+          <h1>{e.name}</h1>
+          <p>{e.description}</p>
+          <button onClick={addHanddle}>add</button>
+          <button onClick={removeHanddle} >remove</button>
+      </div>
+    </>
+  }</div>;
 };
 
 export default ProductItem;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductItem from "./ProductItem";
 const Products = () => {
-  return <div><ProductItem></ProductItem></div>;
+
+  const [pdata,setPdata]=useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:8080/products')
+    .then(response => response.json())
+    .then(data => setPdata(data)); 
+  },[])
+  
+  return <div>
+{pdata.map((e)=>{
+  return(<>
+    <ProductItem e={e}/>
+  </>)
+})}
+
+ 
+  </div>;
 };
 
 export default Products;
